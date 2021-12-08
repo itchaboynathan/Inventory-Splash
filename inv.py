775d37396221d616a113b8d2f8ac7b67abf27d93
Sage <nhorton3728@yahoo.com>
2021-12-08 14:23:14 -0500

    Update inv.py

--- inv.py
+++ inv.py
@@ -2,52 +2,64 @@ import os,sys,json
 from subprocess import call
 from pprint import pprint
 
-#Creates a new inventory item when called
-def build(name,material,size,amount):
-    name = input('[Name?]')
-    material = input('[Material?]')
-    size = input('[Size?]')
-    amount = input('[Amount?]')
-    
-    created_object = Item(name,material,size,amount)
-    json_save(created_object)
-    return created_object
+#Creates a new inventory item when called                                                                                       
+def build(material,size,amount):                                                                                                
+    material = input('[Material?]')                                                                                             
+    size = input('[Size?]')                                                                                                     
+    amount = input('[Amount?]')                                                                                                 
+                                                                                                                                
+    created_object = Item(material,size,amount)                                                                                 
+    json_save(created_object)                                                                                                   
+    return created_object                                                                                                       
+                                                                                                                                
+def json_save(object):                                                                                                          
+    j = json.loads('base.json')                                                                                                 
+    json_data = [object]                                                                                                        
+    with open('base.json', 'a') as f:                                                                                           
+        json_data.append(object.__dict__)                                                                                       
+        json.dumps(json_data, f, ensure_ascii=False, sort_keys=False,  indent=4)                                                
+                                                                                                                                
+def prnt(doc):                                                                                                                  
+    splash = open(doc,'r')                                                                                                      
+    file_contents = splash.read()                                                                                               
+    print(file_contents)                                                                                                        
+                                                                                                                                
+#Blueprint for the inventory item                                                                                               
+class Item(object):                                                                                                                     json_data.append(object.__dict__)
+        json.dumps(json_data, f, ensure_ascii=False, sort_keys=False,  indent=4)
 
-def json_save(object):
-    with open('base.json', 'w', encoding='utf-8') as f:
-        json.dump(object.__dict__, f, ensure_ascii=False, indent=4)
+def prnt(doc):
+    splash = open(doc,'r')
+    file_contents = splash.read()
+    print(file_contents)
 
 #Blueprint for the inventory item
 class Item(object):
-    name = ''
     material = ''
     size = 0
     amount = 0
-    def __init__(self,name,material,size,amount):
-        self.name = name
+    def __init__(self,material,size,amount):
         self.material = material
-        self.size = size 
+        self.size = size
         self.amount = amount
-
+        
 #Handles user input and information output
 class user_interface():
     def clear():
         call('clear' if os.name =='posix' else 'cls')
     def splash():
-        name = ''
         material = ''
         size = 0
         amount = 0
 
-        splash = open('splash.txt','r')
-        file_contents = splash.read()
-        print(file_contents)
+        prnt('splash.txt')
+
         person = input('[Terminal]')
-        
+
         if person  == '1':
             print()
         elif person == '2':
-            build(name,material,size,amount)
+            build(material,size,amount)
         elif person == '3':
             print()
         elif person == '4':
@@ -61,6 +73,7 @@ class user_interface():
 
 face = user_interface
 face.splash()
+
 
 
 
